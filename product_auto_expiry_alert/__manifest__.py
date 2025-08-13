06f299d4010953cf3ad84263259515213c7a7195
Umar <m.umar@skyrocket.com.pk>
2025-08-13 19:19:55 +0500

    Task Name: Product Expiry Alert v18
    Task URL:
    
    Remakrs : NA

---
+++ product_auto_expiry_alert/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Expiry Product Alert",
+    "version": "18.0.0.1",
+    "author": "Muhammad Umar",
+    "description": """Expiry Product Alert""",
+    "category": "Product",
+    "depends": ['product', 'mail'],
+    "data": [
+        'views/product_template_view.xml',
+        'views/product_banner.xml',
+        'data/email_template.xml',
+        'data/cron_expiry_alert.xml'
+    ],
+    "installable": True,
+    "application": False,
+    'license': 'LGPL-3',
+}
